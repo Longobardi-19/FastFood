bedadad4f825e17ce286e861ce6e491de2da83fe
Lily RD <lilyerd23@gmail.com>
2021-08-24 19:48:06 -0500

    salir menu, order activity modificado

--- app/src/main/java/com/aplicaciones/fastfood/MainActivity.kt
+++ app/src/main/java/com/aplicaciones/fastfood/MainActivity.kt
@@ -3,10 +3,13 @@ package com.aplicaciones.fastfood
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.lifecycle.Observer
 import com.aplicaciones.fastfood.databinding.ActivityMainBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,20 +19,48 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        auth = Firebase. auth
+        auth = Firebase.auth
 
-        binding.salir.setOnClickListener{
+        binding.salir.setOnClickListener {
             signOut()
         }
 
-    }
+        var listaOrders = emptyList<Order>()
+        val database = AppDatabase.getDatabase(this)
 
-    private fun signOut(){
+        database.orders().getAll().observe(this, Observer {
+            listaOrders = it
+
+            val adapter = OrderAdapter(this, listaOrders)
+
+            lista.adapter = adapter
+        })
+
+        lista.setOnItemClickListener { parent, view, position, id ->
+            val intent = Intent(this, OrderActivity::class.java)
+            intent.putExtra("id", listaOrders[position].idOrder)
+            startActivity(intent)
+
+        }
+        floatingActionButton.setOnClickListener {
+            val intent = Intent(this, NuevaOrderActivity::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+    private fun signOut() {
         Firebase.auth.signOut()
         val intent = Intent(this, LoginActivity::class.java)
         startActivity(intent)
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.order_menu, menu)
+
+        return super.onCreateOptionsMenu(menu)
     }
 }
--- app/src/main/java/com/aplicaciones/fastfood/OrderActivity.kt
+++ app/src/main/java/com/aplicaciones/fastfood/OrderActivity.kt
@@ -1,13 +1,51 @@
 package com.aplicaciones.fastfood
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class OrderActivity : AppCompatActivity() {
+
+    private lateinit var database: AppDatabase
+    private lateinit var order: Order
+    private  lateinit var orderLiveData: LiveData<Order>
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order)
+
+        database = AppDatabase.getDatabase(this)
+        val idOrder = intent.getIntExtra("id",0)
+
+        orderLiveData = database.orders().get(idOrder)
+
+        orderLiveData.observe(this, Observer {
+            order = it
+            pedido_order.text = order.pedido
+            precio_order.text = "S/.${order.precio}"
+            cantidad_order.text = order.cantidad
+            fecha_order.text = order.fecha
+            cliente_order.text = order.cliente
+            tipoPago_order.text = order.tipoPago
+            notaOrden_order.text = order.notaOrden
+            direccion_order.text = order.direccion
+            referencia_order.text = order.referencia
+            estadoOrden_order.text = order.estadoOrden
+            total_order.text = order.total
+            imagen.setImageResource(order.imagen)
+
+
+        })
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -16,5 +54,24 @@ class OrderActivity : AppCompatActivity() {
         return super.onCreateOptionsMenu(menu)
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.edit_item -> {
+                val intent = Intent(this, NuevaOrderActivity::class.java)
+                intent.putExtra("order", order)
+                startActivity(intent)
+            }
 
+            R.id.delete_item -> {
+                orderLiveData.removeObservers(this)
+
+                CoroutineScope(Dispatchers.IO).launch {
+                    database.orders().delete(order)
+                    this@OrderActivity.finish()
+                }
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
 }
